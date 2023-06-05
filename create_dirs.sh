a178396e3029aca4ecfdaadbb3a9b68bd6a1b033
Floyd-42 <damien-lo@hotmail.fr>
2023-06-05 23:21:50 +0200

    update create_dirs + add qbittorrent + radarr (no config)

--- create_dirs.sh
+++ create_dirs.sh
@@ -23,15 +23,21 @@
 
 
 # Root directory
-root_dir="/volume1/data"
-# root_dir="/home/floyd/projects/volume1/data"
+root_dir="/volume1"
+root_dir="/home/floyd/projects/my-jellyfin/volume1"
+
+# Data directory
+data_dir="$root_dir/data"
+
+# Docker directory
+docker_dir="$root_dir/docker"
 
 # Subdirectories
 sub_dirs=("torrents" "usenet/incomplete" "usenet/complete" "media")
 media_types=("books" "movies" "music" "tv")
 
 # Create root directory
-mkdir -p $root_dir
+mkdir -p $data_dir
 
 # Create subdirectories for torrents, usenet/complete and media
 for dir in "${sub_dirs[@]}"
@@ -39,15 +45,14 @@ do
   if [[ $dir != "usenet/incomplete" ]]; then
     for media_type in "${media_types[@]}"
     do
-      mkdir -p "$root_dir/$dir/$media_type"
+      mkdir -p "$data_dir/$dir/$media_type"
     done
   else
-    mkdir -p "$root_dir/$dir"
+    mkdir -p "$data_dir/$dir"
   fi
 done
 
 
 
 # https://github.com/TRaSH-/Guides-Synology-Templates/blob/main/templates/jellyfin.yml
-sudo mkdir -p -m=00775 /volume1/docker/appdata/{radarr,sonarr,bazarr,jellyfin,pullio,qbittorrent}
-
+mkdir -p $docker_dir/appdata/{radarr,sonarr,bazarr,jellyfin,jellyfin_cache,pullio,qbittorrent}
